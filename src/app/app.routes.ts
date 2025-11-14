4841d0e12257e9e9d42d8cfdbeb5a2326c33efef
PrayasPathak <prayasroman1@gmail.com>
2025-11-14 15:00:47 +0545

    Add functionality to add items to cart

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,4 +14,8 @@ export const routes: Routes = [
     path: 'wishlist',
     loadComponent: () => import('./pages/my-wishlist/my-wishlist').then((m) => m.MyWishlist),
   },
+  {
+    path: 'cart',
+    loadComponent: () => import('./pages/view-cart/view-cart').then((m) => m.ViewCart),
+  },
 ];
